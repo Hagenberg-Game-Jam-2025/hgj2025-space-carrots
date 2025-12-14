1cdce4d7d35398c0e9fe17fee94a5e1b8fba5332
Zoe McFife <cmdrprotogen27@gmail.com>
2025-12-14 13:02:36 +0100

    Screen

---
+++ gay-bunny-in-outer-space/scene/component/rover/Rover.gd
@@ -0,0 +1,13 @@
+# author: zozo
+class_name Rover
+
+extends Node3D
+
+@export
+var mini_games : Array[PackedScene]
+
+var current_mini_game : int = 0
+
+func _ready() -> void:
+	pass
+	
--- gay-bunny-in-outer-space/script/mini-game/mini-game-control-entity/MiniGameControlEntity.gd
+++ gay-bunny-in-outer-space/script/mini-game/mini-game-control-entity/MiniGameControlEntity.gd
@@ -2,17 +2,16 @@ extends ControlEntity
 
 class_name MiniGameControlEntity
 
-@export
 var mini_game : MiniGame
 
-@export
-var mini_game_subviewport : SubViewport
+@onready
+var mini_game_subviewport : SubViewport = $SubViewport
 
-@export
-var mini_game_interaction_area : Area3D
+@onready
+var mini_game_interaction_area : Area3D = $MiniGameQuadMesh/Area3D
 
-@export
-var mini_game_quad_mesh : MiniGameScreenQuadMesh
+@onready
+var mini_game_quad_mesh : MiniGameScreenQuadMesh = $MiniGameQuadMesh
 
 @export
 var interaction_target : InteractionTarget
@@ -24,6 +23,21 @@ func _ready() -> void:
 	possessed.connect(_on_possessed)
 	
 	interaction_target.on_interact.connect(_on_interact)
+
+func load_mini_game(mini_game_scene : PackedScene) -> void:
+	var mini_game_instance : Node = mini_game_scene.instantiate()
+	
+	if !(mini_game_instance is MiniGame):
+		push_error("Packed Scene must be of type MiniGame")
+		return
+	
+	#for (node : Node in mini_game_subviewport.get_children()):
+		#node.
+	
+	mini_game_subviewport.add_child(mini_game_instance)
+	
+	
+	
 
 func _on_possessed(mind : Mind, last_possessed_control_entity : ControlEntity) -> void:
 	player_entity = last_possessed_control_entity
