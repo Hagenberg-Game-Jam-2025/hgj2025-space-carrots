208114928190a7bda5a108e3b71368f026ef6384
Ifynth <s2510238042@fhooe.at>
2025-12-27 05:14:27 +0100

    Fix Death Sound Export

--- gay-bunny-in-outer-space/script/audio/SfxPlayer.gd
+++ gay-bunny-in-outer-space/script/audio/SfxPlayer.gd
@@ -1,16 +1,9 @@
 class_name SfxPlayer
 extends AudioStreamPlayer
 
+@export
+var _death_sound_pool : Array[AudioStream] = []
+
 func play_death_sound() -> void:
-	stream = pick_death_sound()
+	stream = _death_sound_pool.pick_random()
 	play()
-
-func pick_death_sound() -> AudioStreamOggVorbis:
-	return AudioStreamOggVorbis.load_from_file(_death_sound_pool.pick_random())
-
-const _death_sound_pool : Array = [
-	"res://resource/audio/death/death1.ogg",
-	"res://resource/audio/death/death2.ogg",
-	"res://resource/audio/death/death3.ogg",
-	"res://resource/audio/death/death4.ogg",
-]
